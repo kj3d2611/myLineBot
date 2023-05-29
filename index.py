2a12a97dd6d4b3b087726aae41c16cd6671ac703
kj3d2611 <134909473+kj3d2611@users.noreply.github.com>
2023-05-29 15:22:39 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,6 +10,26 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import requests
+
+def checkword(w):
+    url = 'https://www.moedict.tw/uni/' + w
+    r = requests.get(url)
+    datas = r.json()
+    msg = '國字：' + datas['title'] + '\n'
+    msg += '部首：' + datas['radical'] + '\n'
+    msg += '筆劃：' + str(datas['stroke_count']) + '\n\n'
+    for i in range(len(datas['heteronyms'])):
+        msg += '注音：' + datas['heteronyms'][i]['bopomofo'] + '\n'
+        msg += '拼音：' + datas['heteronyms'][i]['pinyin'] + '\n'    
+        for j in range(len(datas['heteronyms'][i]['definitions'])):
+            if 'type' in datas['heteronyms'][i]['definitions'][j]:
+                msg += '[{}] {}\n'.format(
+                    datas['heteronyms'][i]['definitions'][j]['type'],
+                    datas['heteronyms'][i]['definitions'][j]['def'])
+        msg += '\n'
+    return msg
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('/U+H4cEZC+0hMe4N9pUu73MZBMMzPiVRagJMgbTx7im5mOUXJ5G8QBbOILLFhbIeR0Gtoq4IJeSMyMAXwGeQJWPnWRxKHY+Ouzv4oR430QEl3JAIBNGpIEr+/RdyQIphOCnZr7lnR1FWU9LMAsj+FgdB04t89/1O/w1cDnyilFU=')
@@ -39,7 +59,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=checkword(event.message.text)))
 
 
 if __name__ == "__main__":
